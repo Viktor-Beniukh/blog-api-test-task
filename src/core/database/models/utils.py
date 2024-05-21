61a5aab7d5e468efb1f81d0e05e4797449c1e769
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-05-22 00:02:59 +0300

    Removed unnecessary function

--- src/core/database/models/utils.py
+++ src/core/database/models/utils.py
@@ -4,9 +4,3 @@ import re
 def slugify(s):
     pattern = r"[^\w+]"
     return re.sub(pattern, "-", s)
-
-
-def format_tag_name(name: str) -> str:
-    if not name.startswith("#"):
-        return f"#{name.lower()}"
-    return name.lower()
