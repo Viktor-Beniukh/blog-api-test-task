7bf52486ab18e4286556e2923b79e635b0932c7c
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-05-21 19:05:17 +0300

    Added pagination to main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import logging
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
+from fastapi_pagination.utils import disable_installed_extensions_check
 
 from src.core.conf.config import settings
 from src.core.conf.logging_config import setup_logging
@@ -14,6 +16,8 @@ from src.routes import router as router_v1
 setup_logging()
 
 logger = logging.getLogger(__name__)
+
+disable_installed_extensions_check()
 
 
 app = FastAPI(title="Blog API", description="The management of the Blog API")
@@ -33,6 +37,8 @@ def read_root():
 app.include_router(router=auth_router, prefix="/api")
 app.include_router(router=authors_router, prefix="/api")
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+
+add_pagination(app)
 
 
 if __name__ == "__main__":
