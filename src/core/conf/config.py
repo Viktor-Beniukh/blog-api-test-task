f94db89f53e0298529bb426cbe7478dc701e14bb
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-05-24 16:49:40 +0300

    Added db_url and db_url_docker to config.py

--- src/core/conf/config.py
+++ src/core/conf/config.py
@@ -1,18 +1,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from db_url import db_url_docker, db_url
 from src.core.conf.utils import get_app_env
 
 
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
-    database_url: str
-    secret_key: str
-    jwt_secret_key: str
-    algorithm: str
+    database_url: str = db_url_docker if get_app_env() == "prod" else db_url
+    secret_key: str = "secret_key"
+    jwt_secret_key: str = "jwt_secret_key"
+    algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
     refresh_token_expire_minutes: int = 60 * 24 * 7
-    redis_host: str
-    redis_port: str
+    redis_host: str = "host_name"
+    redis_port: str = "port"
 
     model_config = SettingsConfigDict(env_file=get_app_env(), extra="allow")
 
